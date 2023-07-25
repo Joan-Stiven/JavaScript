fe771a500c189b402a12c21350c792138e1d849e
JoanStiven <joanstivensilva12@gmail.com>
2023-07-24 22:12:36 -0500

    se agrego JavaScript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Analisis y Desarrollo de Software");
+
+/* unir dos cadenas de texto
+const nom="Stiven"
+const ape="Silva"
+alert("Mi Nombre Es "+nom+" Y Mi Apellido Es "+ape) */
+
+/*const num1=5
+const num2=2
+const div = num1/num2
+alert("la division es: "+div)*/
+
+let nom=prompt("Esciba sus Nombres y Apellidos)")
+let edad=prompt("Esciba su Edad)")
+let peso=prompt("Esciba su Peso)")
+let dir=prompt("Esciba su Dirreccion)")
+alert("Mi nombreb es "+nom+", tengo "+edad+" años, mi peso es "+peso+" kilos y mi dirrecion es "+dir)
+
+
+
